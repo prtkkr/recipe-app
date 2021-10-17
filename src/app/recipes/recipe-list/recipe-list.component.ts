13a3007507fadcedc7862f6e5797e48c2bd26afb
Pratik Kumar <pratikkumarsbg@gmail.com>
2021-10-17 16:46:14 +0530

    commit

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,14 +8,20 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A Recipe 1', 'Test Description Recipe', 'https://arbuz.com/wp-content/uploads/2009/07/Chicken-Mishwi.jpg'),
-    new Recipe('A Recipe 1', 'Test Description Recipe', 'https://www.archanaskitchen.com/images/archanaskitchen/1-Author/happytrioexplains-gmail.com/Tandoori_Paneer_Samosa.jpg'),
-    new Recipe('A Recipe 1', 'Test Description Recipe', 'https://arbuz.com/wp-content/uploads/2009/07/Chicken-Mishwi.jpg')
+    new Recipe('A Recipe 1', 'Test Description Recipe 1', 'https://arbuz.com/wp-content/uploads/2009/07/Chicken-Mishwi.jpg'),
+    new Recipe('A Recipe 2', 'Test Description Recipe 2', 'https://www.archanaskitchen.com/images/archanaskitchen/1-Author/happytrioexplains-gmail.com/Tandoori_Paneer_Samosa.jpg'),
+    new Recipe('A Recipe 3', 'Test Description Recipe 3', 'https://arbuz.com/wp-content/uploads/2009/07/Chicken-Mishwi.jpg')
   ];
+
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
